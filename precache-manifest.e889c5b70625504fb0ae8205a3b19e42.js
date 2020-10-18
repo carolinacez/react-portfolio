ff5e152090109f8ab03389abcd95fcc3ee9bb049
carolina <carolinaccez@gmail.com>
2020-10-18 11:59:33 -0400

    Updates

---
+++ precache-manifest.e889c5b70625504fb0ae8205a3b19e42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce5b51c28f40226e38db29dde334091",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c48071adce99561805b7",
+    "url": "/react-portfolio/static/css/main.f6e0cfaf.chunk.css"
+  },
+  {
+    "revision": "470e4d1a0b73d50e0314",
+    "url": "/react-portfolio/static/js/2.adb3d2a4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.adb3d2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48071adce99561805b7",
+    "url": "/react-portfolio/static/js/main.4293748e.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "7e16cf5b03634893a4c5ea1408ae28c7",
+    "url": "/react-portfolio/static/media/Resume.7e16cf5b.pdf"
+  },
+  {
+    "revision": "777f4b85abd50c6c14dd1f273b55bf3d",
+    "url": "/react-portfolio/static/media/aboutme.777f4b85.jpg"
+  },
+  {
+    "revision": "cb8137d138c150bd3383863ea6d31f53",
+    "url": "/react-portfolio/static/media/background4.cb8137d1.jpg"
+  },
+  {
+    "revision": "00fb07e14566fa7787807cefeb8ffc87",
+    "url": "/react-portfolio/static/media/pic-1.00fb07e1.jpg"
+  }
+]);
