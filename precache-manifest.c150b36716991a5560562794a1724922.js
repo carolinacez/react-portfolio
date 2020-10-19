2caeea227f5f23f1e12fa75ec750962c35bc5d2a
carolina <carolinaccez@gmail.com>
2020-10-18 22:12:24 -0400

    Updates

--- precache-manifest.c150b36716991a5560562794a1724922.js
+++ precache-manifest.c150b36716991a5560562794a1724922.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "417b77c6058354cbefbf7c2bfe7404fb",
+    "revision": "8851112ebbf11c153e1f21b3a10b04bc",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "d32c0e2c92c270668d6a",
-    "url": "/react-portfolio/static/css/main.9ad00106.chunk.css"
+    "revision": "0e296162d41f2cafbe50",
+    "url": "/react-portfolio/static/css/main.c36363a5.chunk.css"
   },
   {
     "revision": "59e2908d218119530ade",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.acd8e4cc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d32c0e2c92c270668d6a",
-    "url": "/react-portfolio/static/js/main.3aaccda3.chunk.js"
+    "revision": "0e296162d41f2cafbe50",
+    "url": "/react-portfolio/static/js/main.54317499.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
@@ -36,7 +36,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/media/background4.cb8137d1.jpg"
   },
   {
-    "revision": "00fb07e14566fa7787807cefeb8ffc87",
-    "url": "/react-portfolio/static/media/pic-1.00fb07e1.jpg"
+    "revision": "a2ddaf3cf7f3fb3a395aa9ac63c7bdb4",
+    "url": "/react-portfolio/static/media/pic-1.a2ddaf3c.jpg"
+  },
+  {
+    "revision": "1ac9d29a233573596c0587e03d3b6763",
+    "url": "/react-portfolio/static/media/pic-2.1ac9d29a.jpg"
+  },
+  {
+    "revision": "bcb27634e21f2c35c2802b18ddc917f1",
+    "url": "/react-portfolio/static/media/pic-3.bcb27634.jpg"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.3a53817faef95767c4c721f53826f7b0.js"
+  "/react-portfolio/precache-manifest.c150b36716991a5560562794a1724922.js"
 );
 
 self.addEventListener('message', (event) => {
